2a54f2f45e91d9196c06a4b7b66c7b420bf5da53
Alexander Spicer <Alexander.Spicer@hotmail.com>
2026-07-19 14:05:35 -0500

    Include new Discord Webhook API Library.

--- Source/BlacksmithGame/BlacksmithGame.Build.cs
+++ Source/BlacksmithGame/BlacksmithGame.Build.cs
@@ -7,10 +7,10 @@ public class BlacksmithGame : ModuleRules
 	public BlacksmithGame(ReadOnlyTargetRules Target) : base(Target)
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
-	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
 
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
+        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HTTP", "Json", "JsonUtilities" });
+
+        PrivateDependencyModuleNames.AddRange(new string[] {  });
 
         // Uncomment if you are using Slate UI
         // PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
